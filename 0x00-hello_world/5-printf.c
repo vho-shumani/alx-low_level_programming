984ce29e1a651f3c465d8d25a7bc5c74b8c4ce09
shumani <shumanisimeli@gmail.com>
2023-09-07 13:44:30 -0700

    5-printf.c
    mend

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,8 @@
 /**
  * main - Entry point
  *printf - print out the exact string
- *
  * return: Always 0 (success)
  */
- 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
