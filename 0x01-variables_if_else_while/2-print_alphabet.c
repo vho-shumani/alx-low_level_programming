7b44d50df01aa4181858e67585903ffb3567ec60
shumani <shumanisimeli@gmail.com>
2023-09-08 16:35:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the last digit of random number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
