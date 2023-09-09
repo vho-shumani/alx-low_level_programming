d04ef9168f50363faf986bc9cd2a11b62c256a9e
shumani <shumanisimeli@gmail.com>
2023-09-09 08:08:54 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c = 0;
+
+    while (c <= 9)
+    {
+        int C = 0;
+        while (C <= 9)
+        {
+            if (C != c)
+            {
+                putchar('0' + c);
+                putchar('0' + C);
+                putchar(',');
+                putchar(' ');
+                C++;
+            }
+        }
+        c++;
+    }
+    putchar('\n');
+    return 0;
+}
