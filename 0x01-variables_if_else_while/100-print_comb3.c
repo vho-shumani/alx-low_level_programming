2376e491150a57bfa20637f9a29afd6a79ce7948
shumani <shumanisimeli@gmail.com>
2023-09-09 11:55:53 -0700

    100-print_comb3.c
    mend

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
 	int c = 0;
 
 	while (c <= 9)
 	{
 		int C = 0;
+
 		while (C <= 9)
 		{
 			if (C != c && c < C)
 			{
 				putchar('0' + c);
 				putchar('0' + C);
-				putchar(',');
-				putchar(' ');
+				if (c != 8 && C != 9)
+					putchar(',');
+					putchar(' ');
 			}
 			C++;
 		}
 		c++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
