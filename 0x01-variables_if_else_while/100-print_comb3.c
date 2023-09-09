f9b227e6bfae36e4cdf5727a59e832ebe3408236
shumani <shumanisimeli@gmail.com>
2023-09-09 12:24:19 -0700

    100-print_comb3.c
    mend

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main -Entry point
+ *
+ * Description: prints out all possible two digit numbers.
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
 	int c = 0;
@@ -11,7 +17,7 @@ int main(void)
 
 		while (C <= 9)
 		{
-			if (c != C && C < c)
+			if (c != C && c < C)
 			{
 				putchar('0' + c);
 				putchar('0' + C);
