20eb668a759efec51259f56392cf2f19f743072e
shumani <shumanisimeli@gmail.com>
2023-09-10 11:48:45 -0700

    checkup

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,42 +13,7 @@ int main(void)
 
 	while (c <= 9)
 	{
-		int i = 0;
-
-		while (i <= 8)
-		{
-			int x = 0;
-
-			while (x <= 9)
-			{
-			    int f =0;
-
-			    while (f <= 9)
-                	{
-                    		if (x != 0 || f != 0)
-                    		{
-                    		if (x != c || f != i)
-                    		{
-                        	putchar('0' + c);
-                        	putchar('0' + i);
-                        	putchar(' ');
-                       	 	putchar('0' + x);
-                        	putchar('0' + f);
-
-                        	if (c != 9 || i != 8 || x != 9 || f != 9)
-                        	{
-                           	putchar(',');
-                            	putchar(' ');
-                        	}
-                    		}
-                    		}
-
-                    	f++;
-			}
-                	x++;
-			}
-            		i++;
-		}
+		putchar(c)
 		c++;
 	}
     putchar('\n');
