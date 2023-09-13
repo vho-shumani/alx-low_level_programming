f937d7064a0cbd45d628ab0e5b00e4df179b9be9
shumani <shumanisimeli@gmail.com>
2023-09-13 05:13:26 -0700

    program to print time table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+*times_table - prints the times table of 9.
+*
+*
+*Return: returns void.
+*/
+void times_table(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		int b = 0;
+
+		while (b <= 9)
+	{
+		int total = a * b;
+
+		if (total < 10)
+		{
+			_putchar(' ');
+			_putchar(total + '0');
+			b++;
+		}
+		else
+		{
+			_putchar((total / 10) + '0');
+			_putchar((total % 10) + '0');
+			b++;
+		}
+		_putchar(',');
+		_putchar(' ');
+
+	}
+	_putchar('\n');
+	a++;
+	}
+}
