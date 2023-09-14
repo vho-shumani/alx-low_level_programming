74628c950ae7541419f50fee7b2481c7e29ba1d1
shumani <shumanisimeli@gmail.com>
2023-09-14 03:09:42 -0700

    commenting out a infinite loop
    mend

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	/*i = 0;
-
-	while (i < 0)
-	{
-		putchar(i);
-	}*/
-
+	/*
+	*i = 0;
+	*
+	*while (i < 0)
+	*{
+	*	putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
