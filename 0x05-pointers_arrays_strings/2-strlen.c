ed5e10925f2742fe63c4a8c947b43d7e5f26bc35
shumani <shumanisimeli@gmail.com>
2023-09-18 08:02:23 -0700

    counts the length of strin

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - return the total length of a string.
+*@s: the string 
+*
+*
+*Return: returns the length of the string.
+*/
+int _strlen(char *s)
+{
+	int len;
+	
+	len = strlen(s);
+	return(len);
+}
