3c1c7c5d63121837595c750c1202a4ff4466a3ff
shumani <shumanisimeli@gmail.com>
2023-09-18 08:06:34 -0700

    correction for betty code style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 #include <string.h>
 /**
 *_strlen - return the total length of a string.
-*@s: the string 
+*@s: the string.
 *
 *
 *Return: returns the length of the string.
 */
-int _strlen(char *s)
+int _strlen(chae *s)
 {
 	int len;
-	
+
 	len = strlen(s);
-	return(len);
+	return (len);
 }
