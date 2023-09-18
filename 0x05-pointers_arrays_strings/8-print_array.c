65220a9529aa7691ecc0e7ed09e826ea924286c5
shumani <shumanisimeli@gmail.com>
2023-09-18 16:00:10 -0700

    print the element of array
    mend

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,10 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		if (i == (n - 1))
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		else
 			printf("%d, ", a[i]);
 		i++;
 	}
+	print('\n');
 }
