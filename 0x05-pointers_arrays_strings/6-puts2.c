677180ae818ac3ad5c89beed9e6c083e42c6aba6
shumani <shumanisimeli@gmail.com>
2023-09-18 17:46:21 -0700

    print out every other number-ammend

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+*_strlen - return the total length of a string.
+*@s: the string.
+*
+*
+*Return: returns the length of the string.
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+/**
 *puts2 - prints out every other character of the string
 *@str: the string.
 *
@@ -7,10 +26,13 @@
 */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int len = _strlen(str);
+	int i = 0;
+
+	while (i < len)
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
