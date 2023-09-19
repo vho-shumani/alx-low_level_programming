7937df105463ac63cf29dc49748465a9c18eebe3
shumani <shumanisimeli@gmail.com>
2023-09-18 17:50:53 -0700

    prints half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,23 @@
 #include "main.h"
-#include <string.h>
+/**
+*_strlen - return the total length of a string.
+*@s: the string.
+*
+*
+*Return: returns the length of the string.
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
 /**
 *puts_half - prints out only half of the string.
 *@str: the string
@@ -8,7 +26,7 @@
 */
 void puts_half(char *str)
 {
-	int len = strlen(str);
+	int len = _strlen(str);
 	int n = (len / 2);
 
 	if (len % 2 != 0)
