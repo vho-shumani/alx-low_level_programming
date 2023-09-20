18d54adc36fc2b74bddbb05b48a8ac43ec020f10
shumani <shumanisimeli@gmail.com>
2023-09-20 11:51:19 -0700

    append two string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strncat - it appends src string onto dest.
+*@dest: first string
+*@src: second string
+*@n: number of character in src
+*
+*
+*Return: return the appended string.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (i < n && *src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		i++;
+        }
+	return (dest);
+}
