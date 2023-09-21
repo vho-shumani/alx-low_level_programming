940a3a01723a8069682e5709f8afea1712e79499
shumani <shumanisimeli@gmail.com>
2023-09-21 05:30:41 -0700

    4-rev_array.c
    mend

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,6 +12,7 @@ void reverse_array(int *a, int n)
 	int *ptr = a;
 	int x;
 	int i = 0;
+	int diff = n - 1;
 
 	while (i < n)
 	{
@@ -19,6 +20,7 @@ void reverse_array(int *a, int n)
 		*(ptr + i) = *(ptr + diff);
 		*(ptr + diff) = x;
 		n--;
+		diff--;
 		i++;
 	}
 }
