05ba491f59f437a6b3279d0ac7c17c68a0347138
shumani <shumanisimeli@gmail.com>
2023-09-22 08:45:33 -0700

    function fills the first n bytes of the memory area pointed to by s with the constant byte b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,14 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-	int **ptr;
+	unsigned int i = 0;
+	char **ptr;
 
 	ptr = &s;
 	while (i < n)
 	{
-		*(s + i) = '.';
+		*(s + i) = b;
 		i++;
 	}
-	return (ptr);
+	return (*ptr);
 }
