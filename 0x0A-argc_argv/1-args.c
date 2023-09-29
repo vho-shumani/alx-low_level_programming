6284778ca10d96f2a63ceeaff632bdaba0707c9c
shumani <shumanisimeli@gmail.com>
2023-09-29 06:22:09 -0700

    file that prints the number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,13 @@
 /**
 *main - Entry point
 *@argc: number of elements in array.
-*argv: array
+*@argv: array
 *
 *
 *Return: always success(0)
 */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
