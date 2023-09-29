3c4f0063669a41c2ac603dc716bb0b055cc08c22
shumani <shumanisimeli@gmail.com>
2023-09-29 06:06:31 -0700

    fixing betty standard

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc __attribute__((unused)) , char *argv[])
+/**
+*main - Entry point
+*@argc: number of array elements.
+*@argv: array
+*
+*
+*Return: alwats zero on success.
+*/
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
