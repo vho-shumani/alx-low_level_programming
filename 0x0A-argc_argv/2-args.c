5ee10e30b753a0e50c720d8f883b772d5c320f4b
shumani <shumanisimeli@gmail.com>
2023-09-29 09:46:29 -0700

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*@argc: number of elements in array.
+*@argv: array
+*
+*
+*Return: always success(0)
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	} 
+	return (0);
+}
