2a81ee207d006562f47a28256e75cfc17e29e563
shumani <shumanisimeli@gmail.com>
2023-10-02 07:58:14 -0700

    first commit of the projet

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*create_array - creates and initializes an array.
+*@size: the size of the array.
+*@c: characters of array.
+*
+*Return: pointer to array(success)
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	ptr = malloc(sizeof(char) * size);
+	if (size == 0 || *ptr == '\0')
+	{
+		return ('\0');
+	}
+	while(i < size) 
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Project focuses on the use of the malloc and free funtion, and how to use valgrind in c programming.
