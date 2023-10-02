9411f619215d443a9d36a852a24764649f581110
shumani <shumanisimeli@gmail.com>
2023-10-02 08:17:39 -0700

    update on the 0-create_array.c file-ammend

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i = 0;
 
-	ptr = malloc(sizeof(char) * size);
-	if (size == 0 || *ptr == '\0')
+	ptr = (char*)malloc(sizeof(char) * size);
+	if (size == 0 || ptr == NULL)
 	{
 		return ('\0');
 	}
