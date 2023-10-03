a8b356234d595c4be4936aab5165ac264d386a7f
shumani <shumanisimeli@gmail.com>
2023-10-03 08:12:40 -0700

    free a 2d array
    mend

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,16 +3,17 @@
 /**
 *free_grid - frees memory of a 2D array.
 *@grid: the array
-*height: number of arrays.
+*@height: number of arrays.
 *
 *Return: nothing
 */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
 	free(grid);
-} 
+}
