cfa1b4d3469d4b0b6beb91a7bace5b7ed386cc00
shumani <shumanisimeli@gmail.com>
2023-10-08 15:31:03 +0200

    function to multiply two numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point 
+ * @argc: number of argumnets.
+ * @argv: array
+ *
+ * Return: return 0 (success), 98 (fail).
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				exit (98);
+			}
+		}
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+
