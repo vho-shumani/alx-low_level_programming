70efd3f1535b98734da2b0eb8a4307bf85a15ff5
shumani <shumanisimeli@gmail.com>
2023-10-08 11:13:48 +0200

    calloc function
    mend

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,9 +26,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		((int*)ptr)[i] = 0;
+		((char*)ptr)[i] = 0;
 	}
-
-	((int*)ptr)[i] = 0;
 	return (ptr);
 }
