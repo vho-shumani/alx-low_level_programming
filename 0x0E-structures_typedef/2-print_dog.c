301d7f404b5a7ef7653220603e21c21a8e46b4a8
shumani <shumanisimeli@gmail.com>
2023-10-09 17:13:06 +0200

    updating of file and add null statement
    mend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,10 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		exit(0);
+	}
 	if (d->name == NULL)
 	{
 		d->name = "(Nil)";
@@ -17,10 +21,6 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 	{
 		d->owner = "(Nil)";
-	}
-	if (d == NULL)
-	{
-		exit(0);
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
