1efc053ab6ca335a107f9644a8c05716faddc860
shumani <shumanisimeli@gmail.com>
2023-10-09 13:19:40 +0200

    first commit of project

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable type of struct_dog.
+ * @d: variable of struct_dog.
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_h
+#define DOG_h
+/**
+ * struct dog - data type that include char a float type.
+ * @name: first member of type char.
+ * @age: second member of type float.
+ * @owner: third member of type char.
+ *
+ * Description: the structure or type will represent the different detail of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
