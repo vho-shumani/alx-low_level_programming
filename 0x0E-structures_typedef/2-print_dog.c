108c343f722a7d90ee683b3fe6eafb0bca2efd8f
shumani <shumanisimeli@gmail.com>
2023-10-09 16:58:49 +0200

    updating of file and add null statement
    mend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - prints a struct dog
  * @d: pointer to the struct.
@@ -19,7 +20,7 @@ void print_dog(struct dog *d)
 	}
 	if (d == NULL)
 	{
-		return 0;
+		exit(0);
 	}	
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
