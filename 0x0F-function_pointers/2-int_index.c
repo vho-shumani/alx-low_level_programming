6b2ea0f976e76d8acbb347480dff6ba7e97c86a0
shumani <shumanisimeli@gmail.com>
2023-10-11 21:58:39 +0200

    updating 2-int_index.c
    mend

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stddef.h>
 /**
  * int_index - searches for an integer.
  * @array: array
@@ -10,11 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
 	if (size <= 0)
 	{
 		return (-1);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
@@ -22,5 +27,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		}
 	}
-	return (0);
+	return (-1);
 }
