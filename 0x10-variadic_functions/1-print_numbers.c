1421061cc61d6cab16a1b010733c374678ce4652
shumani <shumanisimeli@gmail.com>
2023-10-12 17:59:09 +0200

    updating file 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * print_numbers - prints number from the parameters.
- * @separators: characters that separate the words.
+ * @separator: characters that separate the words.
  * @n: number of intergers passed.
  * Return: nothing
  */
@@ -12,17 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator != NULL && n != 0)
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(list, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(list, int));
-			if ((n - i) != 1)
-				printf("%s", separator);
-		}
-		printf("\n");
+		printf("%d", va_arg(list, int));
+		if ((n - i) != 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
 }
 
 
