ebef3baaa475be6e8e39cde68e4c95bff602013a
shumani <shumanisimeli@gmail.com>
2023-10-19 18:54:34 +0200

    adding the first file for task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of the list_t list
+ * @h: list_t list
+ * Return: the number of nodes (success).
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	size_t num = 0;
+
+	while(temp != NULL)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[0] ");
+			printf("(nil)\n");
+			temp = temp->next;
+			num++;
+		}
+		else
+		{
+			printf("[%d] ", temp->len);
+			printf("%s\n", temp->str);
+			temp = temp->next;
+			num++;
+		}
+	}
+	return (num);
+}
