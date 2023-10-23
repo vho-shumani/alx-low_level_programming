3dea6e4b8b10b0ab122bc4627fae173eba3d4eb4
shumani <shumanisimeli@gmail.com>
2023-10-23 20:18:27 +0200

    8-sum_listint.c
    mend

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,16 +6,17 @@
 */
 int sum_listint(listint_t *head)
 {
-    listint_t *tmp = head;
-    int sum = 0;
-    if (head == NULL)
-    {
-        return (0);
-    }
-    while (tmp != NULL)
-    {
-        sum += tmp->n;
-        tmp = tmp->next;
-    }
-    return (sum);
+	listint_t *tmp = head;
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #ifndef LISTS_H
 #define LISTS_H
 typedef struct listint_s
