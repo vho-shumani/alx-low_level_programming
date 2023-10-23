ca45132c72014fdcf33885b91b6995a142491533
shumani <shumanisimeli@gmail.com>
2023-10-23 23:04:39 +0200

    9-insert_nodeint.c
    mend

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,10 +38,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		tmp = tmp->next;
 	}
-	if (tmp == NULL)
-	{
-		return (NULL);
-	}
 	new->next = tmp->next;
 	tmp->next = new;
 	return (new);
