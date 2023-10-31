866f69d4525481d69cd9cf58d0a29dffc2e49754
shumani <shumanisimeli@gmail.com>
2023-10-31 15:29:15 +0200

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+int create_file(const char *filename, char *text_content)
+{
+    int fptr;
+    int text_len, numbytes_written;
+
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+    if (text_content == NULL)
+    {
+        fptr = open(filename, O_CREAT);
+        close(fptr);
+        return (1);
+    }
+    fptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    text_len = strlen(text_content);
+    numbytes_written = write(fptr, text_content, text_len);
+    close(fptr);
+    if (numbytes_written != text_len)
+    {
+        return (-1);
+    }
+    return (1);
+}
