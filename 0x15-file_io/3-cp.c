1f028b7214b3f4e44d7fb31fd89fba4c8c6ee57d
shumani <shumanisimeli@gmail.com>
2023-11-03 01:06:06 +0200

    3-cp.c
    mend

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,38 +20,43 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit (97);
+		exit(97);
 	}
 	fptr1 = open(argv[1], O_RDONLY);
 	if (fptr1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
-	fptr2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fptr2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fptr2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
-	while ((numbytes_read = read(fptr1, buffer, 1024))> 0)
+	while ((numbytes_read = read(fptr1, buffer, 1024)) > 0)
 	{
 		numbytes_written = write(fptr2, buffer, numbytes_read);
 		if (numbytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit (99);
+			exit(99);
 		}
+	}
+	if (numbytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (close(fptr1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr1);
-		exit (100);
+		exit(100);
 	}
 	if (close(fptr2) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr2);
-		exit (100);
+		exit(100);
 	}
 	return (0);
 }
