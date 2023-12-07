402c402dc4a93d43a9ca5a6cbe87dcdc61ce75c7
shumani <shumanisimeli@gmail.com>
2023-12-07 05:17:19 -0800

    updated 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,40 +1,19 @@
 #include "lists.h"
-
+/**
+* print_dlistint - print all elements in a linked list.
+* @h: pointer to the linked list.
+* Return: number of elements.
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-    dlistint_t *tmp = h;
-    size_t count = 0;
+	const dlistint_t *tmp = h;
+	size_t count = 0;
 
-    while (tmp != NULL)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->next;
-        count++;
-    }
-    return count;
-}
-
-int main(void)
-{
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
 }
