469b1e3da95d21110f38007d82d7ef4a7d9b4805
shumani <shumanisimeli@gmail.com>
2023-12-11 19:37:05 +0200

    updated 1-create_dynamic_lib.sh
    mend

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c -fPIC *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
