af02471d003ac8932a046f8f5643dcf1f5cb49d6
Shumani Simeli <shumanisimeli@gmail.com>
2023-12-23 01:54:43 +0200

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,5 +8,40 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	char *val = strdup(value);
+    unsigned long int index = key_index((const unsigned char*)key, ht->size);
+    hash_node_t *current, *new;
 
+    if (ht == NULL || key == NULL || value == NULL)
+    {
+        return (0);
+    }
+
+    for (current = ht->array[index]; current != NULL; current = current->next)
+    {
+        if(strcmp(current->key, key) == 0)
+        {
+            free(current->value);
+            current->value = strdup(val);
+            free(val);
+            return (1);
+        }
+    }
+    new = (hash_node_t *)malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        free(val);
+        return (0);
+    }
+    new->value = strdup(val);
+    free(val);
+    if (new->value == NULL)
+    {
+        free(new->key);
+        free(new);
+        return(0);
+    }
+    new->next = ht->array[index];
+    ht->array[index] = new;
+    return (1);
 }
