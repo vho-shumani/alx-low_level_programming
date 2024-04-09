30b1de27ff018389d9bd9d2ad516283ca12a5d32
Shumani <shumanisimeli@gmail.com>
2024-04-10 01:26:50 +0200

    updated 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,20 +14,19 @@ int binary_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-	while (start < end)
+	while (start <= end)
 	{
 		mid = (start + end) / 2;
 		printf("Searching in array: ");
 		for (i = start; i <= end; i++)
 		{
-			printf("%d",array[i]);
+			printf("%d", array[i]);
 			if (i != end)
 			{
 				printf(", ");
 			}
-			else
-				printf("\n");
 		}
+		printf("\n");
 		if (array[mid] == value)
 		{
 			return (mid);
@@ -39,7 +38,7 @@ int binary_search(int *array, size_t size, int value)
 		else
 		{
 			end = mid - 1;
-		}	
+		}
 	}
 	return (-1);
 }
